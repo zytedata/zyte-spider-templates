a866976c5f0ee84cbf15f64b38687370953c4791
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2024-01-27 00:10:10 +0800

    simplify get_domain() code

--- zyte_spider_templates/utils.py
+++ zyte_spider_templates/utils.py
@@ -4,6 +4,4 @@ from scrapy.utils.url import parse_url
 
 
 def get_domain(url: str) -> str:
-    allowed_domain = parse_url(url).netloc
-    allowed_domain = re.sub(r"www.*?\.", "", allowed_domain)
-    return allowed_domain
+    return re.sub(r"www.*?\.", "", parse_url(url).netloc)
