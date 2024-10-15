afba5b23ffbf270497f60266e09bde109f9a0e48
Andrey Rakhmatullin <wrar@wrar.name>
2024-10-15 12:56:28 +0500

    Bump tool versions.

--- zyte_spider_templates/documentation.py
+++ zyte_spider_templates/documentation.py
@@ -2,5 +2,5 @@ try:
     from enum_tools.documentation import document_enum
 except ImportError:
 
-    def document_enum(func):
+    def document_enum(func):  # type: ignore[misc]
         return func
